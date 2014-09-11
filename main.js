c6cb5dd8e3ea8c219d6d10108b881d2c65f9a3c7
Jess Scheuring <jscheuring@gmail.com>
2014-09-11 16:10:37 -0400

    implemented ajax for orgs

--- main.js
+++ main.js
@@ -4,25 +4,6 @@ function renderTemplate(templateId, location, array) {
     var renderedTemplate = templateFunction(array);
     $(location).append(renderedTemplate);}
 
-var userData = {
-    image: userData.avatar_url,
-    name: userData.name,
-    username: userData.login,
-    location: userData.location,
-    email: userData.email,
-    joinedDate: moment(userData.created_at).format("MMM D, YYYY"),
-    followers: userData.followers,
-    following: userData.following,
-    starred: userStarredData.length
-  };
-
-var orgData = _.map(userOrgsData, function(org) {
-  return {
-    orgAvatar: org.avatar_url,
-    orgName: org.login,
-    orgUrl: org.url
-  };
-});
 
 $.getJSON('https://api.github.com/users/NicerHugs').done(function(data) {
   var userData = {
@@ -34,13 +15,23 @@ $.getJSON('https://api.github.com/users/NicerHugs').done(function(data) {
       joinedDate: moment(data.created_at).format("MMM D, YYYY"),
       followers: data.followers,
       following: data.following};
-    });
   renderTemplate('#templates-user-links', 'header .content', userData);
   $.getJSON('https://api.github.com/users/NicerHugs/starred').done(function(starredData) {
     userData.starred = starredData.length;
     renderTemplate('#templates-sidebar', '.sidebar', userData);
   });
+});
 
-orgData.forEach(function(orgDatum){
-  renderTemplate('#templates-sidebar-orgs', '.sidebar', orgDatum);
+
+$.getJSON('https://api.github.com/users/NicerHugs/orgs').done(function(data){
+  var orgData = _.map(data, function(org) {
+    return {
+      orgAvatar: org.avatar_url,
+      orgName: org.login,
+      orgUrl: org.url
+    };
+  });
+  orgData.forEach(function(orgDatum){
+    renderTemplate('#templates-sidebar-orgs', '.sidebar', orgDatum);
+  });
 });
